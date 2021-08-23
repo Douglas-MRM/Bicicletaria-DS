0a418ebe81a582df0ee1614179e28cc6bf2f87be
Douglas-MRM <douglaswwaff@gmail.com>
2021-08-23 06:21:48 -0400

    criação das rotas de listagem

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->name('dashboard');
+
+
+// PRODUTOS
+Route::get('/produtos', [Controllers\ProdutosController::class, 'index'])->name('produtos.index');
+
+// SERVIÇOS
+Route::get('/servicos', [Controllers\ServicosController::class, 'index'])->name('servicos.index');
+
+// FUNCIONARIOS
+Route::get('/funcionarios', [Controllers\FuncionariosController::class, 'index'])->name('funcionarios.index');
+
+// CLIENTES
+Route::get('/clientes', [Controllers\ClientesController::class, 'index'])->name('clientes.index');
+
+// FORNECEDORES
+Route::get('/fornecedores', [Controllers\FornecedoresController::class, 'index'])->name('fornecedores.index');
+
+// ESTOQUE
+Route::get('/estoque', [Controllers\EstoqueController::class, 'index'])->name('estoque.index');
