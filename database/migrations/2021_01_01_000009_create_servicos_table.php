b64cdfa77da9455dc767db5e7089cfeea8950fc4
Douglas-MRM <douglaswwaff@gmail.com>
2022-02-16 23:25:07 -0400

    migration da tabela servicos

---
+++ database/migrations/2021_01_01_000009_create_servicos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servicos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->time('tempo');
+            $table->decimal('preco', 8, 2);
+            $table->string('descricao');
+            $table->boolean('ativo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servicos');
+    }
+}
