f96c3bceaebebafc1f70c44cd66e16e3de5bbf97
Douglas-MRM <douglaswwaff@gmail.com>
2022-02-16 23:17:35 -0400

    migration da tabela produtos

---
+++ database/migrations/2021_01_01_000007_create_produtos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('marca');
+            $table->decimal('preco', 8, 2);
+            $table->string('descricao')->nullable();
+            $table->foreignId('categoria_id')->constrained('categorias');
+            $table->foreignId('fornecedor_id')->constrained('fornecedores');
+            $table->boolean('ativo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
