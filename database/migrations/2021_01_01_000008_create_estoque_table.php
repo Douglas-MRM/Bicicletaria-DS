533fbcbf008e30518b0f09e66526c74bb8d84b84
Douglas-MRM <douglaswwaff@gmail.com>
2022-02-16 23:23:25 -0400

    migration da tabela estoque

---
+++ database/migrations/2021_01_01_000008_create_estoque_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstoqueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estoque', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantidade');
+            $table->foreignId('produto_id')->constrained('produtos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estoque');
+    }
+}
