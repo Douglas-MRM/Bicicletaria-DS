3c09484b9d7d06dba702b688bc48cd0e77a9a991
Douglas-MRM <douglaswwaff@gmail.com>
2022-02-16 23:25:57 -0400

    migration da tabela clientes

---
+++ database/migrations/2021_01_01_000010_create_clientes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->date('data_nascimento');
+            $table->string('email')->unique();
+            $table->string('cpf', 11)->unique();
+            $table->string('rg');
+            $table->string('endereco');
+            $table->foreignId('sexo_id')->constrained('sexos');
+            $table->boolean('ativo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
