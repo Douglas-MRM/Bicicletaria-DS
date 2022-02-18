5455395b0cbec64341e08435015c643576a38780
Douglas-MRM <douglaswwaff@gmail.com>
2022-02-17 23:24:50 -0400

    add ao model funcionarios o relacionamento com a tabela sexos e cargos

--- app/Models/Funcionarios.php
+++ app/Models/Funcionarios.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Model;
 
 class Funcionarios extends Model
@@ -11,4 +12,14 @@ class Funcionarios extends Model
 
     protected $table = 'funcionarios';
     protected $guarded = [];
+
+    public function sexo(): HasOne
+    {
+        return $this->hasOne(Sexos::class, 'id', 'sexo_id');
+    }
+
+    public function cargo(): HasOne
+    {
+        return $this->hasOne(Cargos::class, 'id', 'cargo_id');
+    }
 }
